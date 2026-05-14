c7da4dfc8e74c707ad268998f9c7faa2e954e392
willz-0 <arrolsoffack@gmail.com>
2026-05-14 23:58:33 +0100

    Update main.js

--- element3/main.js
+++ element3/main.js
@@ -110,10 +110,7 @@ box.material.diffuseColor = new BABYLON.Color3(1, 0, 0);
 
         if (distanceToBox < 3.5) {
           const pushDirection = box.position.subtract(player.position).normalize();
-          box.physicsImpostor.applyImpulse(
-    pushDirection.scale(2),
-    box.getAbsolutePosition()
-);
+         box.position.addInPlace(pushDirection.scale(0.08));
         
       });
     }

e2915043c64443a9a3af33a5e9d34c8d77650e96
willz-0 <arrolsoffack@gmail.com>
2026-05-14 22:26:50 +0100

    Update main.js

--- element3/main.js
+++ element3/main.js
@@ -119,8 +119,12 @@ groundMat.diffuseTexture.vScale = 10;
         if (inputMap["s"]) player.moveWithCollisions(player.forward.scale(-speed));
         if (inputMap["a"]) player.rotate(BABYLON.Vector3.Up(), -0.05);
         if (inputMap["d"]) player.rotate(BABYLON.Vector3.Up(), 0.05);
-      });
-    }
+  const distanceToBox = BABYLON.Vector3.Distance(player.position, box.position);
+
+if (distanceToBox < 3.5) {
+  const pushDirection = box.position.subtract(player.position).normalize();
+  box.position.addInPlace(pushDirection.scale(0.08));
+  )
   );
 
   return scene;

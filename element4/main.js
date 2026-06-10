8c3c28bf6c40140ac91f4737113f3f742b25124a
willz-0 <arrolsoffack@gmail.com>
2026-06-10 12:42:13 +0100

    Update main.js

--- element4/main.js
+++ element4/main.js
@@ -167,7 +167,7 @@ const createScene = () => {
         const distanceToBox = BABYLON.Vector3.Distance(player.position, box.position);
 
    if (distanceToBox < 2.2) {
-    health -= 1;
+    health -= 0.1;
     health = Math.max(health, 0);
     healthBar.width = (health * 2) + "px";
 }

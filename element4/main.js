c6d1ef70946bb039a6b1e7445a7268675867903c
willz-0 <arrolsoffack@gmail.com>
2026-06-10 12:17:26 +0100

    Update main.js

--- element4/main.js
+++ element4/main.js
@@ -86,7 +86,7 @@ const createScene = () => {
     const guiTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
     const panel = new BABYLON.GUI.Rectangle();
-    panel.width = "300px";
+    panel.width = "200px";
     panel.height = "30px";
     panel.cornerRadius = 5;
     panel.color = "white";

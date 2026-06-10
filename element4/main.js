2d2f19a70d8b64a441f1a18b30b11bc3e1db231c
willz-0 <arrolsoffack@gmail.com>
2026-06-10 12:05:52 +0100

    Update main.js

--- element4/main.js
+++ element4/main.js
@@ -175,7 +175,7 @@ const createScene = () => {
         const distanceToHealBox = BABYLON.Vector3.Distance(player.position, healBox.position);
 
 if (distanceToHealBox < 2.2) {
-    health = 200;
+    health = 500;
     healthBar.width = "200px";
     
         }
